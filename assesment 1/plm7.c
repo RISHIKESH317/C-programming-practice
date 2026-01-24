32ade5fac68a48cfcfc4a235b15657db51ab3ace
RISHIKESH317 <rishikeshrsq@gmail.com>
2026-01-24 17:06:17 +0530

    pl7

---
+++ assesment 1/plm7.c
@@ -0,0 +1,15 @@
+//Get a two-digit number from the user and print the ten's digit.
+
+#include<stdio.h>
+
+int main()
+{
+    int a,c;
+    printf("Enter the value :");
+    scanf("%d",&a);
+    c =a/10;
+    printf("answer :%d",c);
+    return 0;
+    
+    
+}
