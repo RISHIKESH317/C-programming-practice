092b2d1006d904e138eb825b0d3295d4a6f02ae0
RISHIKESH317 <rishikeshrsq@gmail.com>
2026-01-26 17:27:23 +0530

    pl17

---
+++ assesment 1/plm17.c
@@ -0,0 +1,16 @@
+//Get a two-digit number from the user, make the one's digit 0, then print it.
+
+#include<stdio.h>
+
+int main()
+{
+    int a; 
+    printf("Enter the value :");
+    scanf("%d",&a);
+    a/=10;
+    a=(a*10)
+    printf("The result is :%d",a);
+    return 0;
+    
+} 
+
