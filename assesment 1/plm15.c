90c0b7a46de6ade9c677ccca2a96ac92d5e98fc4
RISHIKESH317 <rishikeshrsq@gmail.com>
2026-01-26 17:08:15 +0530

    plm43

---
+++ assesment 1/plm15.c
@@ -0,0 +1,18 @@
+//Get a four-digit number from the user and print the reverse of the number.
+
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c,d; 
+    printf("Enter the value :");
+    scanf("%d",&a);
+    b=(a%10)*10;
+    c=b+(a/10)%10;
+    d=(a/100)*100;
+    d=d+c;
+    printf("The result is :%d",d);
+    return 0;
+    
+} 
+
