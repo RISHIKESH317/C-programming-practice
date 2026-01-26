9e98d0a123399bd1ae67e65981ef8047d97c3db2
RISHIKESH317 <rishikeshrsq@gmail.com>
2026-01-26 15:29:27 +0530

    plm9

---
+++ assesment 1/plm9.c
@@ -0,0 +1,15 @@
+//Get a three-digit number from the user and print the hundred's digit.
+
+#include<stdio.h>
+
+int main()
+{
+    int a,c;
+    printf("Enter the value :");
+    scanf("%d",&a);
+    c =a/100;
+    printf("answer :%d",c);
+    return 0;
+    
+    
+}
