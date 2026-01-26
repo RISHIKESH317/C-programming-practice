38adb5bb3063800a5af1d542d12fa60f9947a722
RISHIKESH317 <rishikeshrsq@gmail.com>
2026-01-26 15:41:33 +0530

    plm

---
+++ assesment 1/plm8.c
@@ -0,0 +1,15 @@
+// Get a three digit number from user and print one's digit
+
+#include<stdio.h>
+
+int main()
+{
+    int a,c;
+    printf("Enter the value :");
+    scanf("%d",&a);
+    c =a%10;
+    printf("answer :%d",c);
+    return 0;
+    
+    
+}
