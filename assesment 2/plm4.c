e97a30643633aff5c37ac4fd5b1d207aba6e385a
RISHIKESH317 <rishikeshrsq@gmail.com>
2026-01-27 10:33:31 +0530

    plm4

---
+++ assesment 2/plm4.c
@@ -0,0 +1,11 @@
+//Get a number from the user and check if the number greater than 50. If yes, the print 1, otherwise print 0
+
+
+#include <stdio.h>
+int main (){
+    int a;
+    printf("Enter the number :");
+    scanf("%d",&a);
+    printf("The result is :%d",a>50);
+    return 0;
+}
