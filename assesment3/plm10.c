2dc3ef670c2ad80f3ab695ae5974fed15407e7aa
RISHIKESH317 <rishikeshrsq@gmail.com>
2026-01-27 11:21:49 +0530

    if/else

---
+++ assesment3/plm10.c
@@ -0,0 +1,18 @@
+//Get a two-digit number from the user and check if the digit 0 is greater than the digit 1.
+//If yes, print "Success"; otherwise, print "Failure"
+
+#include <stdio.h>
+int main (){
+    int a,b,c;
+    printf("Enter the number :");
+    scanf("%d",&a);
+    b=a%10;
+    c=a/10;
+    if(b>c){
+   printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+    return 0;
+}
