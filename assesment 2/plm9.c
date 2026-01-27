0a634aa4f29a63a0793d5f21405264081b59d6e1
RISHIKESH317 <rishikeshrsq@gmail.com>
2026-01-27 10:42:26 +0530

    plm9

---
+++ assesment 2/plm9.c
@@ -0,0 +1,11 @@
+//Get a two-digit number from the user and check if the digit 0 less than digit 1  If yes, print 1; otherwise, print 0
+
+
+#include <stdio.h>
+int main (){
+    int a;
+    printf("Enter the number :");
+    scanf("%d",&a);
+    printf("The result is :%d",(a%10)<(a/10));
+    return 0;
+}
