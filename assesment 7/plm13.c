aaf6b4c2c7a55ecf0768b71a00facc9fb151141f
RISHIKESH317 <rishikeshrsq@gmail.com>
2026-01-31 11:43:20 +0530

    function without return

--- assesment 7/plm13.c
+++ assesment 7/plm13.c
@@ -1,19 +1,19 @@
-//sum of digits
+//reverse the number
 
 #include<stdio.h>
-void disp_sum_all_digit(int a){
+void disp_reverse_number(int a){
     int sum=0,b;
     while(a!=0){
         b=a%10;
-        sum=sum+b;
+        sum=(sum*10)+b;
         a/=10;
     }
-    printf("The sum of digit :%d",sum);
+    printf("The reverse of digit :%d",sum);
 }
 
 int main(){
     int a;
     printf("enter the number :");
     scanf("%d",&a);
-     disp_sum_all_digit(a);
+     disp_reverse_number(a);
 }
