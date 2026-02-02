6231bee3fb69efcbbd03c093152180c569419fa2
RISHIKESH317 <rishikeshrsq@gmail.com>
2026-02-02 11:13:08 +0530

    function  return

---
+++ assesment 8/plm1.c
@@ -0,0 +1,18 @@
+//loop to add
+
+#include<stdio.h>
+
+int disp_sum(){
+    int sum=0;
+    for(int i=1; i<=5; i++){
+        sum=sum+i;
+    }
+    return sum;
+}
+
+int main(){
+    int result;
+    result = disp_sum();
+    printf("%d",result);
+    return 0;
+}
